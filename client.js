0b57bd34833c56907541b635fe23322e463df941
PalBotics <p.lydick@gmail.com>
2025-04-25 12:02:21 -0400

    simplified and added settings.html

--- client.js
+++ client.js
@@ -1,26 +1,21 @@
-console.log("Trello Power-Up: client.js loaded");
-
 window.TrelloPowerUp.initialize({
-  'card-buttons': function(t, options) {
-    console.log("Trello Power-Up: initializing card button");
+  'card-buttons': function(t) {
     return [{
       text: 'Test CSV Button',
       callback: function(t) {
-        console.log("Trello Power-Up: button clicked");
         return t.popup({
-          title: 'Hello from CSV',
+          title: 'Import CSV',
           url: 'popup.html',
-          height: 180
+          height: 200
         });
       }
     }];
   },
 
-  'show-settings': function(t, options) {
-    console.log("Trello Power-Up: opening settings");
+  'show-settings': function(t) {
     return t.popup({
-      title: 'CSV Importer Settings',
-      url: 'popup.html',
+      title: 'Settings',
+      url: 'settings.html',
       height: 200
     });
   }
